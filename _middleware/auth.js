4d001ada1885f791e4376a126f1b7d1259316658
Damera Sagar <damera.sagar@impetus.co.in>
2019-09-11 13:04:14 +0530

    DEMO APP

---
+++ _middleware/auth.js
@@ -0,0 +1,31 @@
+const jwt = require('../_service/jwt');
+
+const isWebValid = (req, res, next) => {
+    let token = req.headers['authorization'] || req.params.token || req.query.token;
+    if (token) {
+        let decoded = jwt.verifyWebToken(token);
+        if (decoded) {
+            if (req.method === 'POST') {
+                req.body.created_by = decoded._id
+            } else if (req.method === 'PUT') {
+                req.body.updated_by = decoded._id
+            }
+            req.session = decoded;
+            next();
+        } else {
+            res.status(401).json({
+                success: false,
+                message: 'Invalid Token'
+            })
+        }
+    } else {
+        res.status(401).json({
+            success: false,
+            message: 'Token not found'
+        });
+    }
+}
+
+module.exports = {
+    isWebValid
+}
